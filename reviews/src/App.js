20792a449afb59c3da84b79483e382a0693d805b
lora lameh <loralameh@gmail.com>
2022-02-23 15:15:24 +0200

    reviews project

---
+++ reviews/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Review from './Review'
+function App() {
+  return (
+    <main>
+      <section>
+        <div className='container'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+      </section>
+      <Review />
+    </main>
+  )
+}
+
+export default App
